b340ec9553999b29f13c9be71ec57d49006a51b2
Pierre Adel <pierreadelkamel@gmail.com>
2021-01-24 04:34:46 +0200

    create react app

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# ihola-front
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <h1>Welcome to iHola!</h1>
+    </div>
+  );
+}
+
+export default App;
