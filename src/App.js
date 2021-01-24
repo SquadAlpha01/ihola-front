7fd10aac45e09923fa461173c340cd3c98b8b5d3
Ramez Noshy <RamezNoshy@gmail.com>
2021-01-24 14:10:28 +0200

    feat: project structure
    Co-authored-by: Pierre <pierreadelkamel@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import classes from './App.module.css';
+import classes from "./App.module.css";
 
 function App() {
   return (
     <div className={classes.App}>
-      <h1>Welcome to iHola!</h1>
+      <h1>Welcome to iHola!, alphaSquad</h1>
     </div>
   );
 }
