923876716a8ed232d9fbc8eb7f9a4b71b895797d
Ramez <rameznoshy@gmail.com>
2021-01-27 19:33:38 +0200

    feat: message style and status

--- src/components/activeChat/messageArea/message/message.js
+++ src/components/activeChat/messageArea/message/message.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import classes from './message.module.css';
+import {ReactComponent as MessageSeen} from '../../../../assets/message_status_vectors/msgseen_icon.svg'
+import {ReactComponent as MessageSent} from '../../../../assets/message_status_vectors/msgsent_icon.svg'
 
 const message = (props) => (
-    <div className={classes.message}>
-        {props.content}
+    <div className={classes.messageBackground}>
+        <p className={classes.messageContent}>
+            {props.content}
+        </p>
+        <div className={classes.messageStatus}>
+            <span className={classes.date}>
+                10:30 pm
+            </span>
+            <span className={classes.receivingStatus}>
+                <MessageSent height='20px' width='20px'></MessageSent>
+            </span>
+        </div>
+
     </div>
 );
 
