d04f2ad59e7d30073ad88578880c3bdd9bfc3f27
Ramez <rameznoshy@gmail.com>
2021-01-27 18:19:04 +0200

    feat: header styling

--- src/components/activeChat/header/header.js
+++ src/components/activeChat/header/header.js
@@ -2,7 +2,11 @@ import React from 'react'
 import classes from './header.module.css'
 
 const header = (props) => {
-    return (<div>header</div>)
+    return (
+        <div class={classes.header}>
+            {props.receiverName}
+        </div>
+    )
 }
 
 export default header
