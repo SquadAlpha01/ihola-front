a723961624d35af32a806c677d5194b76ccd2cbc
Ramez <rameznoshy@gmail.com>
2021-01-31 05:49:21 +0200

    feat: change send message icon

--- src/components/activeChat/newMessage/newMessage.js
+++ src/components/activeChat/newMessage/newMessage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import classes from './newMessage.module.css';
+import {ReactComponent as SendIcon} from '../../../assets/images/send_icon.svg'
 
+const MessageSeenIcon = <span className={classes.receivingStatus}> 
+                            <SendIcon ></SendIcon>
+                        </span>
 const newMessageContainer = (props) => (
     <div className={classes.newMessageContainer}>
         <textarea className={classes.newMessageTextField} id="newMessageTextField" name="newMessageTextField" rows="1" placeholder="Enter Message ...">
@@ -8,6 +12,7 @@ const newMessageContainer = (props) => (
         <button 
             className={classes.sendMessageButton} 
             onClick={props.sendMessage}>
+            <SendIcon width="30px" height="30px"></SendIcon>
         </button>
     </div>
 );
