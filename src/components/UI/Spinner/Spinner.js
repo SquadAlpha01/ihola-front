76f2965b3c6f355b18d9b34f82290c68423ceee0
Pierre Adel <pierreadelkamel@gmail.com>
2021-02-02 20:44:28 +0200

    Created spinner UI component

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import classes from './Spinner.module.css'
+export default function Spinner() {
+    return (
+        <div className={classes.Loader}>Loading...</div>
+    )
+}
