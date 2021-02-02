9122e2461240ef7fece72704ede90141a5e77e48
Ramez <rameznoshy@gmail.com>
2021-02-02 12:33:09 +0200

    feat: navbar implementation

--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -1,10 +1,14 @@
 import { React } from 'react'
 import classes from './sideBar.module.css';
 import Settings from '../Settings/Settings'
+import ChatsBar from '../chatSidebar/chatSidebar';
+import Contacts from '../contactsSidebar/contactsSidebar';
 
 const sideBar = (props) => (
     <div className={classes.sideBar}>
-       <Settings/>
+        {props.openStatus.isSettingsOpen ? <Settings></Settings> : null}
+        {props.openStatus.isChatsOpen ? <ChatsBar></ChatsBar> : null}
+        {props.openStatus.isContactsOpen ? <Contacts></Contacts> : null}
     </div>
 );
 
