95e83907c91221357597bf40b9e70e435e86ec16
Pierre Adel <pierreadelkamel@gmail.com>
2021-02-02 20:47:33 +0200

    Added thunk, redux, axios and react-redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
- 
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk'
+
+import findUserReducer from './store/reducers/findUser'
+
+const rootReducer = combineReducers({
+  findUser: findUserReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
